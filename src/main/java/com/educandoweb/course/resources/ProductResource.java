b5a50433cca50feb6ff6f9825fb04ae268bcf1ef
Marco <mass7813@gmail.com>
2023-10-26 17:12:09 -0300

    many to many jointable

--- src/main/java/com/educandoweb/course/resources/ProductResource.java
+++ src/main/java/com/educandoweb/course/resources/ProductResource.java
@@ -1,9 +1,7 @@
 package com.educandoweb.course.resources;
 
 import com.educandoweb.course.entites.Product;
-import com.educandoweb.course.entites.Products;
 import com.educandoweb.course.service.ProductService;
-import com.educandoweb.course.service.ProductsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
