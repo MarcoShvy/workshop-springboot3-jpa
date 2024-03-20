06faeb940b99bb91d9f03ea23df7d4926a4c8ae7
Marco <mass7813@gmail.com>
2024-03-19 23:49:42 -0300

    Exception not found

--- src/main/java/com/educandoweb/course/service/UserService.java
+++ src/main/java/com/educandoweb/course/service/UserService.java
@@ -2,6 +2,7 @@ package com.educandoweb.course.service;
 
 import com.educandoweb.course.entites.User;
 import com.educandoweb.course.repository.UserRepository;
+import com.educandoweb.course.service.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = userRepository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
